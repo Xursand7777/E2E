69a5434e6694f9f39decbbfbfa74c13e205d58e1
xursand7777 <khursand.khaitbaev@mail.ru>
2021-09-28 15:38:43 +0500

    tested events

--- src/app/counter/counter.component.spec.ts
+++ src/app/counter/counter.component.spec.ts
@@ -1,6 +1,7 @@
 import {CounterComponent} from "./counter.component";
 import {ComponentFixture, TestBed} from "@angular/core/testing";
 import {By} from "@angular/platform-browser";
+import {trigger} from "@angular/animations";
 
 describe('CounterComponent', () => {
   let component: CounterComponent
@@ -23,9 +24,28 @@ describe('CounterComponent', () => {
     let num = 42
     component.counter = num
     fixture.detectChanges()
-   let de = fixture.debugElement.query(By.css('.counter'))
+    let de = fixture.debugElement.query(By.css('.counter'))
      let el : HTMLElement = de.nativeElement
 
     expect(el.textContent).toContain(num.toString())
   });
+
+  it('should add green class if counter is even ',  () => {
+      component.counter = 6
+      fixture.detectChanges()
+    let de = fixture.debugElement.query(By.css('.counter'))
+    let el : HTMLElement = de.nativeElement
+    expect(el.classList.contains('green')).toBeTruthy()
+
+  });
+
+
+  it('should increment counter if increment button was clicked ', () => {
+      let btn = fixture.debugElement.query(By.css('#increment'))
+      btn.triggerEventHandler('click', null)
+
+    expect(component.counter).toBe(1)
+  });
+
+
 })
